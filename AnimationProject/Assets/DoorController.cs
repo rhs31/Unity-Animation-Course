c2e82e537ce1c160d7fb80c25ca6054313d1c53b
rhs31 <r.silverberg@yahoo.com>
2018-09-12 16:34:52 -0400

    Added spacedoor w/ animation/trigger, reorganized folders

---
+++ AnimationProject/Assets/DoorController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour {
+    Animator anim;
+    private void OnTriggerEnter(Collider obj)
+    {
+        anim.SetBool("isOpening", true);
+    }
+    private void OnTriggerExit(Collider obj)
+    {
+        anim.SetBool("isOpening", false);
+    }
+    // Use this for initialization
+    void Start () {
+        anim = this.transform.parent.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
