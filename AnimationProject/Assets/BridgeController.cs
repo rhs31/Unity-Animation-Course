5f90fc958b666ab4e02b71ffad3af0fa78b94e56
rhs31 <r.silverberg@yahoo.com>
2018-09-12 18:08:42 -0400

    Added drawbridge scene

---
+++ AnimationProject/Assets/BridgeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeController : MonoBehaviour {
+
+	public Animator anim;
+
+	void OnTriggerEnter(Collider obj)
+	{
+		anim.SetBool("open",true);
+	}
+
+	void OnTriggerExit(Collider obj)
+	{
+		anim.SetBool("open",false);
+	}
+
+}
