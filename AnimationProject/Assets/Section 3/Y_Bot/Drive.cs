03a482f01b4cbd8529eb0dc9df30740c49fa8f10
rhs31 <r.silverberg@yahoo.com>
2018-09-12 23:58:55 -0400

    Added walking/idle animation

---
+++ AnimationProject/Assets/Section 3/Y_Bot/Drive.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drive : MonoBehaviour {
+    float speed = 5.0f;
+    float rotationSpeed = 5.0f;
+    Animator anim;
+
+    private void Start()
+    {
+        anim = gameObject.GetComponent<Animator>(); // could also use keyword "this" instead of gameObject
+    }
+
+    // Update is called once per frame
+    void Update () {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        translation *= Time.deltaTime;
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+
+        if(translation != 0)
+        {
+            anim.SetBool("isWalking", true);
+            anim.SetFloat("characterSpeed", translation); // this makes the animation to play backwards when walking backwards (animation plays at the rate of translation).
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+            anim.SetFloat("characterSpeed", 0);
+        }
+	}
+}
