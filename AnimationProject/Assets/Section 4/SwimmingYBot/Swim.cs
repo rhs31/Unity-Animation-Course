a0e9d20aa98e36dc632ec571b41b8e47840a634f
rhs31 <r.silverberg@yahoo.com>
2018-09-14 17:03:06 -0400

    Reorganized Section 4, added swimming scene

---
+++ AnimationProject/Assets/Section 4/SwimmingYBot/Swim.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// THIS IS CALLED "DRIVE" IN THE LECTURE
+
+public class Swim : MonoBehaviour {
+
+    float speed = 5.0F;
+    float rotationSpeed = 100.0F;
+    float currentSpeed = 0.0f;
+    Animator anim;
+
+    void Start()
+    {
+    	anim = this.GetComponent<Animator>();
+    }
+
+    void Update() 
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        
+        currentSpeed += translation;
+        if(currentSpeed > 5) currentSpeed = 5;
+        anim.SetFloat("Speed", currentSpeed);
+        transform.Rotate(0, rotation, 0);
+
+        if(translation != 0)
+        {
+        	anim.SetBool("isSwimming",true);
+        }
+        else
+        {
+        	anim.SetBool("isSwimming",false);
+            currentSpeed = 0;
+        }
+    }
+}
---
+++ AnimationProject/Assets/Section 4/WalkingYBot/Move.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// THIS SCRIPT IS CALLED "DRIVE" IN THE VIDEO, IT IS "MOVE" HERE TO SEPARATE IT FROM THE OTHER SCENE'S "DRIVE" SCRIPT.
+
+public class Move : MonoBehaviour {
+
+    float speed = 5.0f;
+    float rotationSpeed = 100.0f;
+    Animator anim;
+
+    private void Start()
+    {
+        anim = gameObject.GetComponent<Animator>(); // could also use keyword "this" instead of gameObject
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+
+        transform.Rotate(0, rotation, 0);
+
+        if (translation != 0)
+        {
+            anim.SetBool("isWalking", true);
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+        }
+        
+    }
+}
